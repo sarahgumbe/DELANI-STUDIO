22b0550e1da2bb2c945f11b273c122ab4f46ff8b
sarahgumbe <sarah.gumbe@moringaschool.com>
2022-11-25 21:27:15 +0300

    added bootstrap

--- js/script.js
+++ js/script.js
@@ -104,3 +104,20 @@ $("#overlay8").show();
 $("#overlay8").hide();
 });
 });
+//form functionality
+$(document).ready(function(){
+    $("form#form").submit(function(event){
+      // event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
